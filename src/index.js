4c4b65005e8b90d4f8dfeb597fc0dcd4163d206b
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2023-05-26 10:08:33 +0100

    adjust receipt schema ahead of savings functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-exports.calculator = (basket) => {
+exports.calculator = (basket, offers = []) => {
     return basket.reduce(({receipt, total}, item, i) => {
         const isLastItem = i === basket.length -1
         const currentTotal = +total + +item.price;
-        const totalAsPrice = currentTotal.toFixed(2)
-        const currentReceipt = [...receipt, item]
+        const totalAsPrice = currentTotal.toFixed(2);
 
-        if (isLastItem) currentReceipt.push({item: "Total to pay", price: totalAsPrice})
+        const currentReceipt = {...receipt, items: [...receipt.items, item]};
+
+        //offers
+        if (offers.length) {
+
+        }
+
+        if (isLastItem) currentReceipt.totals.push({item: "Total to pay", price: totalAsPrice})
 
         return {receipt: currentReceipt, total:totalAsPrice};
-    }, {receipt:[], total:''})
+    }, {receipt:{items:[], savings:[], totals:[]}, total:''})
 }
--- test/index.test.js
+++ test/index.test.js
@@ -9,14 +9,25 @@ describe('calculator', () => {
     })
     test('should list totaled items from basket', () => {
         const basket = [{ item: 'beans', price: '0.50' }]
-        const receipt = [{
-            item: 'beans',
-            price: '0.50'
-        },
-        {
-            item: 'Total to pay',
-            price: '0.50'
-        }]
+        const receipt = {
+            items:[{
+                item: 'beans',
+                price: '0.50'
+            }],
+            savings:[],
+            totals:[{
+                item: 'Total to pay',
+                price: '0.50'
+            }]}
         expect(calculator(basket).receipt).toEqual(receipt);
+    })
+    describe('offers', () => {
+        test('should apply multi-buy savings', () => {
+            const offers = [{ item: 'beans', basePrice: "0.50", discountName: "2 for 1", discountValue: "50", discountUnit: "%" }]
+            const basket = [{ item: 'beans', price: '0.50' }]
+            const basket2 = [{ item: 'beans', price: '0.50' }, { item: 'beans', price: '0.50' }]
+            expect(calculator(basket, offers).total).toBe('0.50');
+            expect(calculator(basket2, offers).total).toBe('0.50');
+        })
     })
 })
