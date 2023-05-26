746d41d40085e476ceb8ecc5d5979d4790c0f2fe
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2023-05-26 12:12:26 +0100

    refactor for readability and immutability

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-const calculateDiscount = (offer, items) => {
-    const { basePrice,offerMultiplier, offerCondition} = offer;
-    const numberOfNonEligibleItems = items.length % offerCondition;
-    const eligibleItemsN = items.length - numberOfNonEligibleItems;
-    const discount = eligibleItemsN  * offerMultiplier * basePrice
-    return discount
-}
+const {calculateSavings} = require('./offers')
+
 
 exports.calculator = (basket, offers = []) => {
     const basketTotal =  basket.reduce(({receipt, total}, item, i) => {
-        const isLastItem = i === basket.length -1
         const currentTotal = +total + +item.price;
         const totalAsPrice = currentTotal.toFixed(2);
 
@@ -21,25 +15,14 @@ exports.calculator = (basket, offers = []) => {
     const subTotalItem = {item: 'Sub-total', price: basketTotal.total}
     basketTotal.receipt.totals.push(subTotalItem)
 
-    offers.forEach(offer => {
-       const { item, basePrice,offerName,offerValue, offerType,offerUnit, offerCondition, } = offer;
-       const basketHasOfferItem = !!basketTotal.receipt.items.find(product => product.item ===item)
-       if (basketHasOfferItem) {
+    const {savings, totalSavings} = calculateSavings(offers, basketTotal);
 
-        // check eligibility of items for offer
-        const eligibleItems = basketTotal.receipt.items.filter(basketItem => basketItem.item === item );
-        const conditionMet = eligibleItems.length >= offerCondition;
-        const discount = calculateDiscount(offer, eligibleItems);
-        // apply offer & ammend receipt
-        if (discount > 0) {
-            basketTotal.total = (+basketTotal.total - discount).toFixed(2)
-            basketTotal.receipt.savings.push({item: offerName, price: discount.toFixed(2)})
-        }
-       };
-    });
+    
+    if (totalSavings) basketTotal.receipt.totals.push({item:'Total Savings', price: totalSavings.toFixed(2) })
+    if (totalSavings) basketTotal.total =  (basketTotal.total - totalSavings).toFixed(2)
+    if (savings.length) basketTotal.receipt.savings = savings;
 
-    if (offers.length) basketTotal.receipt.totals.push({item:'Total Savings', price: (+subTotalItem.price - +basketTotal.total).toFixed(2) })
     basketTotal.receipt.totals.push({item:'Total to pay', price:basketTotal.total })
-    console.log(JSON.stringify(basketTotal, null, 2))
+
     return basketTotal
 }
---
+++ src/offers.js
@@ -0,0 +1,35 @@
+const calculateDiscount = (offer, items) => {
+    const { basePrice,offerMultiplier, offerCondition} = offer;
+
+    // presuming offer conditions are based on minimum units of product.
+    const numberOfNonEligibleItems = items.length % offerCondition;
+    const eligibleItemsN = items.length - numberOfNonEligibleItems;
+    
+    const discount = eligibleItemsN  * offerMultiplier * basePrice
+    return discount
+}
+
+const getOfferElligibleItems = (items, offerItem) => items.filter(({item}) => item === offerItem );
+
+exports.calculateSavings = (offers, basketTotal) => {
+    return offers.reduce((offerAcc, offer) => {
+    const { item, offerName } = offer;
+
+    const basketHasOfferItem = !!basketTotal.receipt.items.find(product => product.item ===item);
+    if (!basketHasOfferItem) return offerAcc;
+    if (basketHasOfferItem) {
+
+     // check eligibility of items for offer
+     const eligibleItems = getOfferElligibleItems(basketTotal.receipt.items, item);
+
+     const discount = calculateDiscount(offer, eligibleItems);
+
+     if (discount > 0) {
+         const savingItem = {item: offerName, price: discount.toFixed(2)}
+         return {savings: [...offerAcc.savings, savingItem], totalSavings: offerAcc.totalSavings + discount}
+     };
+
+     return offerAcc
+    };
+ }, {savings: [], totalSavings: 0 })
+}
