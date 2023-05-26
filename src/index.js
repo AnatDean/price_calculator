0a160ab1cf42fc4a02a34084296feebbb6f4c51c
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2023-05-26 09:52:46 +0100

    add total row to receipt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 exports.calculator = (basket) => {
-    return basket.reduce(({receipt, total}, item) => {
+    return basket.reduce(({receipt, total}, item, i) => {
+        const isLastItem = i === basket.length -1
         const currentTotal = +total + +item.price;
+        const totalAsPrice = currentTotal.toFixed(2)
         const currentReceipt = [...receipt, item]
-        return {receipt: currentReceipt, total:currentTotal.toFixed(2)}
+
+        if (isLastItem) currentReceipt.push({item: "Total to pay", price: totalAsPrice})
+
+        return {receipt: currentReceipt, total:totalAsPrice};
     }, {receipt:[], total:''})
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,22 @@
-const {calculator}= require('../src/index');
+const { calculator } = require('../src/index');
 
 describe('calculator', () => {
     test('should add up simple supermaket items without offers or by weight pricing', () => {
-        const basket = [{item: 'beans', price: '0.50'}]
-        const basket2 = [{item: 'beans', price: '0.50'}, {item: 'beans', price: '0.50'}, {item: 'coke', price: '0.70'}]
+        const basket = [{ item: 'beans', price: '0.50' }]
+        const basket2 = [{ item: 'beans', price: '0.50' }, { item: 'beans', price: '0.50' }, { item: 'coke', price: '0.70' }]
         expect(calculator(basket).total).toBe('0.50');
         expect(calculator(basket2).total).toBe('1.70');
     })
     test('should list totaled items from basket', () => {
-        const basket = [{item: 'beans', price: '0.50'}]
+        const basket = [{ item: 'beans', price: '0.50' }]
         const receipt = [{
             item: 'beans',
             price: '0.50'
-    }]
+        },
+        {
+            item: 'Total to pay',
+            price: '0.50'
+        }]
         expect(calculator(basket).receipt).toEqual(receipt);
     })
 })
