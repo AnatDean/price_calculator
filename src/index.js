b263827e59b3172a366355c4a9479e02f5188fe8
Anat Dean <anatdean@Anats-MacBook-Pro.local>
2023-05-29 21:34:11 +0100

    update index sample file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const {calculator} = require('./calculator')
 
-const sampleBasket = [{ item: 'beans', price: '0.50' }, { item: 'beans', price: '0.50' }, { item: 'beans', price: '0.50' }, { item: 'bananas', price: '1.99', weight: '0.5kg', priceModifier: 'kg' }]
+const sampleBasket = [{ item: 'beans', price: '0.50' }, { item: 'beans', price: '0.50' }, { item: 'beans', price: '0.50' }, { item: 'bananas', price: '1.99', weight: '0.5kg', priceModifier: 'kg' }, { item: 'coke', price: '1.50' },{ item: 'coke', price: '1.50' }]
 
-const sampleOffers = [{ item: 'beans', basePrice: "0.50", offerName: "2 for 1", offerType:'unit',offerMultiplier: 0.5,offerCondition:2 }]
+const sampleOffers = [{ item: 'beans', basePrice: "0.50", offerName: "2 for 1", offerType:'unit',offerMultiplier: 0.5,offerCondition:2 }, { item: 'coke', basePrice: "1.50", offerName: "2 for £2", offerType:'fixed',offerMultiplier: (2/3),offerCondition:2 }]
 
 const calculatedBasket = calculator(sampleBasket, sampleOffers)
 
